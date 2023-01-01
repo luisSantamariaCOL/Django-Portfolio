39d53f73f06f356b8c7425bf601915741237dbbc
luisSantamariaCol <luisestebansantamaria@outlook.com>
2023-01-01 09:10:27 -0500

    Project model created

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)  # title of the project
+    description = models.CharField(max_length=250)  # description of the project
+    image = models.ImageField(upload_to="portfolio/images/")  # image of the project
+    url = models.URLField(blank=True)  # url of the project
+
