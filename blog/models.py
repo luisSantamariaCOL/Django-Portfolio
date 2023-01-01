ebc125f38e24bc0b08729468f292eeef4c2e7aad
luisSantamariaCol <luisestebansantamaria@outlook.com>
2023-01-01 12:35:00 -0500

    Post model created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import datetime
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)  # title of the post
+    description = models.TextField()
+    image = models.ImageField(upload_to='blog/images/')
+    date = models.DateField(datetime.date.today)
