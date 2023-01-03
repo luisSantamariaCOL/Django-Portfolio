a3a56329ae708ac49f3b40721346b762869f4294
luisSantamariaCol <luisestebansantamaria@outlook.com>
2023-01-03 10:11:44 -0500

    blog.post_detail view created

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.render_posts, name='posts'),
+    path('<int:post_id>/', views.post_detail, name='post')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
+
 
 # Create your views here.
 def render_posts(request):
     posts = Post.objects.all() # get all posts from database
     return render(request, 'posts.html', {'posts': posts})
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'post_detail.html', {'post': post})
